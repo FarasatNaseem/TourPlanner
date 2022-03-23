92e4768124aa5a284c0fec5d7848cc2705865465
Muhammad Farasat Hussain <If20b048@technikum-wien.at>
2022-03-23 06:00:29 +0100

    Command class added.

---
+++ TourPlannerFrontend/ViewModel/Commands/BaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TourPlannerFrontend.ViewModel.Commands
+{
+    public class BaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _execute;
+        private Predicate<object> _canExecute;
+
+        public BaseCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public void Execute(object parameter) => _execute.Invoke(parameter);
+    }
+}
