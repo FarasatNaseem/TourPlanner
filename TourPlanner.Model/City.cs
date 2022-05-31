b4c758a235ab478d4601f18deb4de1ce26fc96bc
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2022-05-31 18:24:54 +0200

    Added City.json file

---
+++ TourPlanner.Model/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Model
+{
+    class City
+    {
+        public string Name { get; set; }
+
+        public City(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
--- TourPlanner.Model/Constraint.cs
+++ TourPlanner.Model/Constraint.cs
@@ -2,6 +2,6 @@
 {
     public class Constraint
     {
-        public const string BASEURL = @"C:\Users\Privat\TourPlanner\";
+        public const string BASEURL = @"C:\Users\user\Desktop\TourplannerRepo2\TourPlanner\";
     }
 }
